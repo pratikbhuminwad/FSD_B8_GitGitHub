c3bd7abef0bb6df2d13b7905f24a408e4896b194
Pratik Bhuminwad <bhuminwadpratik753@gmail.com>
2025-07-30 17:49:21 +0530

    first commit

--- first.java
+++ first.java
@@ -1,5 +1,5 @@
 public class first {
     public static void main (string[] args) {
-        system.out.println(x:"hello from first class");
+        system.out.println(x:"hello from first class today");
     }
 }
