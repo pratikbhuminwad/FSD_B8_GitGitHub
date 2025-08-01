deffed4782a406c73ea64f33bfe46c837ade0502
pratikbhuminwad <bhuminwadpratik753@gmail.com>
2025-08-01 18:19:44 +0530

    Update second.java

--- second.java
+++ second.java
@@ -2,5 +2,7 @@ public class first {
     public static void main (string[] args) {
         system.out.println(x:"hello from second class");
         //pratik
+        int a = 5;
+        
     }
 }
